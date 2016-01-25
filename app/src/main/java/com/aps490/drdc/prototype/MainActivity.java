c9d2d2b18ca03119dccfc767c6462e8e9942cb3a
Ammar <ammarj965@gmail.com>
2016-01-24 22:01:55 -0500

    Working successfully on Epson

--- app/src/main/java/com/aps490/drdc/prototype/MainActivity.java
+++ app/src/main/java/com/aps490/drdc/prototype/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.wikitude.architect.ArchitectView;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
