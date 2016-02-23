41aabda97ea204221a9368b4672b1981baff1fc9
Ammar <ammarj965@gmail.com>
2016-02-23 17:59:57 -0500

    HTTP connecting client instead of websocket as LEAP motion wants....

--- app/src/main/java/com/aps490/drdc/prototype/LeapService.java
+++ app/src/main/java/com/aps490/drdc/prototype/LeapService.java
@@ -1,16 +1,17 @@
 package com.aps490.drdc.prototype;
 
 import android.app.IntentService;
-import android.app.Service;
 import android.content.Intent;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.os.ResultReceiver;
+import android.os.AsyncTask;
 import android.util.Log;
 
-import java.text.SimpleDateFormat;
-import java.util.Timer;
-import java.util.TimerTask;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LeapService extends IntentService {
 
@@ -31,6 +32,7 @@ public class LeapService extends IntentService {
 
         if (run) {
             Log.d("AMMAR", "Service: " + run);
+            connectToServer();
         }
 
         broadcastGestureAction();
@@ -43,5 +45,57 @@ public class LeapService extends IntentService {
         broadcastIntent.putExtra("tapx", 23);
         broadcastIntent.putExtra("tapy", 174);
         sendBroadcast(broadcastIntent);
+    }
+
+    private void connectToServer() {
+        (new RetrieveLeapMotionDataTask()).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+    private class RetrieveLeapMotionDataTask extends
+            AsyncTask<Void, String, Void> {
+        protected Void doInBackground(Void... args) {
+            try {
+                URL url = new URL(getString(R.string.leap_endpoint));
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                Log.i("AMMAR", "connected");
+
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
+
+                JSONObject data = new JSONObject();
+                JSONObject response;
+
+                data.put("focused", "true");
+                data.put("background", "true");
+                data.put("enableGestures", "true");
+                Log.i("AMMAR", data.toString());
+
+                OutputStreamWriter request = new OutputStreamWriter(urlConnection.getOutputStream());
+                Log.i("AMMAR", "Request part");
+                request.write(data.toString());
+                request.flush();
+                request.close();
+                String line = "";
+                InputStreamReader isr = new InputStreamReader(urlConnection.getInputStream());
+                BufferedReader reader = new BufferedReader(isr);
+                StringBuilder sb = new StringBuilder();
+                while ((line = reader.readLine()) != null)
+                {
+                    sb.append(line + "\n");
+                }
+                // Response from server after login process will be stored in response variable.
+                response = new JSONObject(sb.toString());
+
+                Log.i("AMMAR", "Response: " + sb.toString());
+
+                Log.i("AMMAR","end of loop");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
     }
 }
