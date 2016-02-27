0aaf0f76e21de350142be0b60fb161317fcff992
Ammar <ammarj965@gmail.com>
2016-02-27 17:23:53 -0500

    Swipe and direction processing implemented.

--- app/src/main/java/com/aps490/drdc/prototype/LeapActionReceiver.java
+++ app/src/main/java/com/aps490/drdc/prototype/LeapActionReceiver.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import org.json.JSONObject;
+
 /**
  * Created by JollyRancher on 16-02-21.
  */
@@ -25,6 +27,33 @@ public class LeapActionReceiver extends BroadcastReceiver {
 
     private void processPayLoad(Intent intent) {
         String payload = intent.getStringExtra("payload");
-        Log.d(Constants.TAG, "[" + this.getClass().getSimpleName() + "]" + payload);
+        //Log.d(Constants.TAG, "[" + this.getClass().getSimpleName() + "]" + payload);
+
+        try {
+            JSONObject frame = new JSONObject(payload);
+
+
+            // Gestures
+            if (frame.getJSONArray("gestures").length() != 0) {
+                JSONObject gesture = frame.getJSONArray("gestures").getJSONObject(0);
+
+                String gestureType = gesture.getString("type");
+
+                switch (gestureType){
+                    case "swipe":
+                        Double swipeDirectionX = gesture.getJSONArray("direction").getDouble(0);
+                        if (swipeDirectionX > 0) {
+                            Log.i(Constants.TAG, "You swiped to the right! Open menu");
+                        } else if (swipeDirectionX <= 0) {
+                            Log.i(Constants.TAG, "You swiped to the left! Close menu");
+                        }
+                        break;
+                    default:
+                        Log.i(Constants.TAG, "Gesture new recognized yet!");
+                }
+            }
+        } catch (Exception e) {
+            Log.e(Constants.TAG, e.toString());
+        }
     }
 }
