aa32748037a4c92804ede405b5594d1934ae95a3
Claudezyx <claude.zhao@mail.utoronto.ca>
2016-03-09 21:35:09 -0500

    back_arrow

--- app/src/main/java/com/aps490/drdc/prototype/ModuleSelection.java
+++ app/src/main/java/com/aps490/drdc/prototype/ModuleSelection.java
@@ -1,8 +1,10 @@
 package com.aps490.drdc.prototype;
 
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 
@@ -14,10 +16,14 @@ public class ModuleSelection extends AppCompatActivity implements Communications
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_module_selection);
 
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
         String message_username = intent.getStringExtra(LoginActivity.USER_NAME);
         String message_password = intent.getStringExtra(LoginActivity.PASSWORD);
+
+
     }
 
     public void openMainPage(View view) {
@@ -25,6 +31,30 @@ public class ModuleSelection extends AppCompatActivity implements Communications
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+/*
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings){
+            return true;
+        }
+        if(id==android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    */
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case android.R.id.home:
+                Intent homeIntent = new Intent(this, LoginActivity.class);
+                homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
+        }
+        return (super.onOptionsItemSelected(menuItem));
+    }
+
 
     public void openSubAssemblyPage(View view) {
         // Do something in response to button
