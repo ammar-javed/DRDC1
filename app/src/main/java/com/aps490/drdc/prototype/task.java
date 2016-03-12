3234266e285990674807124f7789a4fbefe0d613
EzgiYigit <ezgi.yigit@utoronto.ca>
2016-03-12 13:01:24 -0500

    Fixed merge conflicts

--- app/src/main/java/com/aps490/drdc/prototype/task.java
+++ app/src/main/java/com/aps490/drdc/prototype/task.java
@@ -30,25 +30,17 @@ public class task extends AppCompatActivity implements AdapterView.OnItemClickLi
         String moduleName = intent.getStringExtra("name");
         System.out.println(moduleName);
 
-<<<<<<< HEAD
         try{
             Assembly assembly = new Assembly( getAssets().open(CourseModules.map.get(moduleName) ) );
             values = assembly.getModules().toArray(new String[0]);
             System.out.println(values);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
             l = (ListView) findViewById(R.id.listView);
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_view_layout,R.id.list_content,values);
             l.setAdapter(adapter);
             l.setOnItemClickListener(this);
-=======
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        l = (ListView) findViewById(R.id.listView);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_view_layout,R.id.list_content,values);
-        l.setAdapter(adapter);
-        l.setOnItemClickListener(this);
->>>>>>> e3347f4bd4ec06de8b3c767c7d6116d57486657b
-
         }
         catch(IOException e){
             e.printStackTrace();
@@ -57,17 +49,10 @@ public class task extends AppCompatActivity implements AdapterView.OnItemClickLi
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-<<<<<<< HEAD
         Intent newIntent = new Intent(this, instructions.class);
         newIntent.putExtra("name",intent.getStringExtra("name"));
         newIntent.putExtra("task",position);
         startActivity(newIntent);
-=======
-        Intent intent = new Intent(this, Main2Activity.class);
-        intent.putExtra("position",intent.getIntExtra("position",0));
-        intent.putExtra("task", position);
-        startActivity(intent);
->>>>>>> e3347f4bd4ec06de8b3c767c7d6116d57486657b
 
     }
 
