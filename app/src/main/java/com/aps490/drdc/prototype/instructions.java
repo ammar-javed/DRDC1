24897873bde9df983f49c092d0f2f2a5f0a8e2bf
EzgiYigit <ezgi.yigit@utoronto.ca>
2016-03-12 12:29:14 -0500

    Added module selection dynamically.

--- app/src/main/java/com/aps490/drdc/prototype/instructions.java
+++ app/src/main/java/com/aps490/drdc/prototype/instructions.java
@@ -12,48 +12,54 @@ import android.widget.TextView;
 import java.io.IOException;
 
 public class instructions extends AppCompatActivity {
+    Assembly assembly;
+    Instruction currentInstr;int task;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+
+        System.out.println("Creating instructions activity.");
+        String assemblyName = intent.getStringExtra("name");
+        task = intent.getIntExtra("task", 0);
+        System.out.println("Acquired intent values.");
+
         setContentView(R.layout.activity_instructions);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-    }
-
-   /* public void getNextInstruction(View view) {
-
-        Assembly assembly;
-        Instruction instruction;
-
         try {
-            assembly = new Assembly(getAssets().open("mainLandingGearWheel.xml"));
-            instruction = assembly.getInstr();
-            ((TextView) findViewById(R.id.textViewInst)).setText(instruction.getText());
+            System.out.println("Attempting to load assembly " + assemblyName);
+            Assembly assembly = new Assembly( getAssets().open(CourseModules.map.get(assemblyName) ) );
+            System.out.println("Initialized assembly: " + assemblyName);
+            assembly.selectModule(task);
+            System.out.println("Module selected: " + assembly.getModules().get(task));
         }
         catch(IOException e){
             e.printStackTrace();
         }
 
     }
-   /* Intent intent = getIntent();
-    int position = intent.getIntExtra("position", 0);
-    int task = intent.getIntExtra("task", 0);
-    Assembly assembly;
-    Instruction instruction;
+
+    public void getPreInstruction(View view) {
+        Instruction instruction = assembly.getPreviousInstr();
+        if(instruction!=null)
+            currentInstr = instruction;
+
+        ((TextView) findViewById(R.id.textViewInst)).setText(currentInstr.getText());
+
+    }
+
+
 
     public void getNextInstruction(View view) {
+        Instruction instruction = assembly.getInstr();
+        if(instruction!=null)
+            currentInstr = instruction;
 
+        ((TextView) findViewById(R.id.textViewInst)).setText(currentInstr.getText());
 
-        try {
-            assembly = new Assembly(getAssets().open("mainLandingGearWheel.xml"));
-            instruction = assembly.getInstr();
-            ((TextView) findViewById(R.id.textViewInst)).setText(instruction.getText());
-        }
-        catch(IOException e){
-            e.printStackTrace();
-        }
-
-    }*/
+    }
 }
--- app/src/main/java/com/aps490/drdc/prototype/task.java
+++ app/src/main/java/com/aps490/drdc/prototype/task.java
@@ -12,28 +12,42 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.IOException;
+
 public class task extends AppCompatActivity implements AdapterView.OnItemClickListener {
     ListView l ;
 
-    String[] values = {"Removal","Installation"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        String[] values;
+        Intent intent = getIntent();
+        String moduleName = intent.getStringExtra("name");
+        System.out.println(moduleName);
 
-        l = (ListView) findViewById(R.id.listView);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_view_layout,R.id.list_content,values);
-        l.setAdapter(adapter);
-        l.setOnItemClickListener(this);
+        try{
+            Assembly assembly = new Assembly( getAssets().open(CourseModules.map.get(moduleName) ) );
+            values = assembly.getModules().toArray(new String[0]);
+            System.out.println(values);
+            l = (ListView) findViewById(R.id.listView);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_view_layout,R.id.list_content,values);
+            l.setAdapter(adapter);
+            l.setOnItemClickListener(this);
 
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent intent = new Intent(this, Main2Activity.class);
-        intent.putExtra("position",intent.getIntExtra("position",0));
+        Intent intent = new Intent(this, instructions.class);
+        intent.putExtra("name",intent.getStringExtra("name"));
         intent.putExtra("task",position);
         startActivity(intent);
 
